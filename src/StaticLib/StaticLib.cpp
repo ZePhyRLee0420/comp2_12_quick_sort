f737fd339a3ad03af033698c0e14f52013382f0e
ZePhyRLee0420 <aice123456789@gmail.com>
2026-01-13 19:37:04 +0900

    test passed

--- src/StaticLib/StaticLib.cpp
+++ src/StaticLib/StaticLib.cpp
@@ -7,6 +7,43 @@
 bool quick_sort(item* begin, const item* end)
 {
 	// ToDo: クイックソートで並び替えてください
+    if (begin == NULL || end == NULL)
+        return false;
 
-	return false;
+    if (end < begin)
+        return false;
+
+    if (end - begin <= 1)
+        return true;
+
+    item* left = begin;
+    item* right = (item*)end - 1;
+
+    item pivot = begin[(end - begin) / 2];
+
+    while (left <= right)
+    {
+        while (left->key < pivot.key)
+            left++;
+
+        while (right->key > pivot.key)
+            right--;
+
+        if (left <= right)
+        {
+            item tmp = *left;
+            *left = *right;
+            *right = tmp;
+
+            left++;
+            right--;
+        }
+    }
+
+    if (begin < right + 1)
+        quick_sort(begin, right + 1);
+    if (left < end)
+        quick_sort(left, end);
+
+    return true;
 }
